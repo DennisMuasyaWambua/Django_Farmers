93b6bd3a1c113e1c77aab6eda370a954043ea856
Dennis wambua <muasyathegreat4@gmail.com>
2024-08-30 08:16:16 +0300

    all hosts allowed

--- FarmerDjango/FarmerDjango/settings.py
+++ FarmerDjango/FarmerDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6&0w#t+x@iuekd-b3203g^v+jut*d*=n^xl0$sv%%pfd#-yssm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
