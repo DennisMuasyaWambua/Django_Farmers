fb4dc7eb8defeccad72a380c2d78592c48927e0b
Dennis wambua <muasyathegreat4@gmail.com>
2024-11-16 13:50:28 +0300

    crop frontend added to the list

--- FarmerDjango/FarmerDjango/settings.py
+++ FarmerDjango/FarmerDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6&0w#t+x@iuekd-b3203g^v+jut*d*=n^xl0$sv%%pfd#-yssm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',"https://cropfrontend-production.up.railway.app/"]
 
 
 # Application definition
